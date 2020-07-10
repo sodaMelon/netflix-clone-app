43fd32eef5f35a4bf09e22c517b0778058a76dbd
sodam2 <sodamlee315@gmail.com>
2020-07-10 17:51:36 +0900

    홈 화면에 이미지 캐로셀 슬라이더 위젯 추가

--- netflix_clone/lib/screen/home_screen.dart
+++ netflix_clone/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/model/model_movie.dart';
+import 'package:netflix_clone/widget/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -8,27 +9,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<Movie> movies = [
     Movie.fromMap({
-      'title': '사랑의 불시착',
+      'title': '사랑의 불시착1',
       'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
+      'poster': 'img/test_movie_1.png',
       'like': false
     }),
     Movie.fromMap({
-      'title': '사랑의 불시착',
+      'title': '사랑의 불시착2',
       'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
+      'poster': 'img/test_movie_1.png',
       'like': false
     }),
     Movie.fromMap({
-      'title': '사랑의 불시착',
+      'title': '사랑의 불시착3',
       'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
+      'poster': 'img/test_movie_1.png',
       'like': false
     }),
     Movie.fromMap({
-      'title': '사랑의 불시착',
+      'title': '사랑의 불시착4',
       'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
+      'poster': 'img/test_movie_1.png',
       'like': false
     }),
   ];
@@ -39,7 +40,16 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            CarouselImage(movies: movies),
+            TopBar(),
+          ],
+        )
+      ],
+    );
   }
 }
 
